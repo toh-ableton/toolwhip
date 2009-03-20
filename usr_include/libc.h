d1aa1d2fd041fef647f5472b558242bfeaba3338
mark <mark@chromium.org>
2009-03-20 20:42:44 +0000

    Reorganize usr_include into usr_include_maker, and provide a prebuilt usr_include.

---
+++ usr_include/libc.h
@@ -0,0 +1,15 @@
+#ifndef _LIBC_H
+#define _LIBC_H
+
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/param.h>
+
+#endif
